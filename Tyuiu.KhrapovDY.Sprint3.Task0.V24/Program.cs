ea1409e432f9c67cb4e210843651e7691c2d364e
Daniil Khrapov <Tyuiu.KhrapovDY@outlook.com>
2024-10-23 16:35:40 +0500

    Task0

---
+++ Tyuiu.KhrapovDY.Sprint3.Task0.V24/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.KhrapovDY.Sprint3.Task0.V24.Lib;
+
+namespace Tyuiu.KhrapovDY.Sprint3.Task0.V24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Храпов Д. Ю. | СМАРТб-24-1";
+            
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                         *");
+            Console.WriteLine("* Тема: Оператор цикла for                                                          *");
+            Console.WriteLine("* Задание #0                                                                        *");
+            Console.WriteLine("* Вариант #24                                                                       *");
+            Console.WriteLine("* Выполнил: Храпов Даниил Юрьевич | СМАРТб-24-1                                     *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                          *");
+            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет произведение             *");
+            Console.WriteLine("* ряда по формуле, при X = 5.                                                       *");
+            Console.WriteLine("*                                                                                   *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                  *");
+            Console.WriteLine("*************************************************************************************");
+
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 7;
+            
+            Console.WriteLine("Переменная X = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                        *");
+            Console.WriteLine("*************************************************************************************");
+
+            Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(value, startValue, stopValue)); 
+        }
+    }
+}
