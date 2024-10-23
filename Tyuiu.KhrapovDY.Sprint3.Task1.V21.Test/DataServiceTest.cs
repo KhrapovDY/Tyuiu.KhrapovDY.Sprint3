5c10eca95bdcd7bbaf4b5d0e357f99008ff66f4a
Daniil Khrapov <Tyuiu.KhrapovDY@outlook.com>
2024-10-23 17:42:53 +0500

    Task1

---
+++ Tyuiu.KhrapovDY.Sprint3.Task1.V21.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+using Tyuiu.KhrapovDY.Sprint3.Task1.V21.Lib;
+
+namespace Tyuiu.KhrapovDY.Sprint3.Task1.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+
+            int value = 1;
+            int startValue = 1;
+            int stopValue = 12;
+
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+
+            double wait = 98.339;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KhrapovDY.Sprint3.Task1.V21/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.KhrapovDY.Sprint3.Task1.V21.Lib;
+
+namespace Tyuiu.KhrapovDY.Sprint3.Task1.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Храпов Д. Ю. | СМАРТб-24-1";
+            
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                         *");
+            Console.WriteLine("* Тема: Оператор цикла while                                                        *");
+            Console.WriteLine("* Задание #1                                                                        *");
+            Console.WriteLine("* Вариант #21                                                                       *");
+            Console.WriteLine("* Выполнил: Храпов Даниил Юрьевич | СМАРТб-24-1                                     *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                          *");
+            Console.WriteLine("* Написать программу используя цикл while, которая вычисляет произведение           *");
+            Console.WriteLine("* ряда по формуле, при х = 1.                                                       *");
+            Console.WriteLine("*                                                                                   *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                  *");
+            Console.WriteLine("*************************************************************************************");
+
+            int value = 1;
+            int startValue = 1;
+            int stopValue = 12;
+
+            Console.WriteLine("Переменная X = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                        *");
+            Console.WriteLine("*************************************************************************************");
+
+            Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(value, startValue, stopValue));
+        }
+    }
+}
